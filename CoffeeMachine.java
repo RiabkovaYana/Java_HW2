9ec218b2aa74b0f032ef9249ab80a2029cc719a4
RiabkovaYana <123588181+RiabkovaYana@users.noreply.github.com>
2023-03-06 05:13:52 -1200

    Add files via upload
    Извиняюсь, но к сожалению не хватило идей, чтобы дойти до конца...

---
+++ CoffeeMachine.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название напитка: ");
+        int drink = scanner.nextInt();
+        scanner.close();
+
+
+        switch (drink) {
+
+            case 1:
+                System.out.println("Эспрессо");
+                break;
+
+            case 2:
+                System.out.println("Американо");
+                break;
+
+            case 3:
+                System.out.println("Капучино");
+                break;
+
+            case 4:
+                System.out.println("Tea");
+                break;
+
+            default:
+                System.out.println("Ошибка. Некорректный ввод.");
+        }
+
+            public static void CoffeeMachine() {
+            String a = "Приготовление эспрессо";
+            String b = "Добавление воды";
+            String с = "Добавление вспененного молока";
+            String d = "Выбор пакетика";
+
+            public static String function CoffeeMachine() {
+                return CoffeeMachine();
+
+
+                }
+            }
+        }
+
+    private static void CoffeeMachine() {
+    }
+}
